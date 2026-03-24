004480042d6e1ce81c2859f56e3af625c7ed1e26
Pollyana-Vieira <adenicecandidadossantosvieira@gmail.com>
2026-03-24 00:18:41 +0000

    criação de arquivo

--- BANCODIGITAL/Controlador.java
+++ BANCODIGITAL/Controlador.java
@@ -4,6 +4,10 @@ class Controlador {
     public static void criarConta(){
         Scanner sc = new Scanner(System.in);
         
+        Nome n = new nome;
+        CPF c = new CPF;
+        Data d = new dataDeNascimento;
+
         //cabeçalho
         System.out.println("-------SEJA BEM-VINDO---------");
         System.out.println("====BANCO DIGITAL====");
@@ -11,16 +15,18 @@ class Controlador {
         //coletar os dados
         System.out.println("Digite seu nome completo: ");
         String nome = sc.nextLine();
-         setNome(String nome);
+        n.setNome();
+
+
 
         System.out.println("Digite seu CPF: ");
         String CPF = sc.nextLine();
-        setCPF(String CPF);
+        c.setCPF();
         
 
         System.out.println("Digite sua data de nascimento: ");
         String data = sc.nextLine();
-        setDataDeNascimento(String dataDeNascimento);
+        d.setDataDeNascimento();
         
 
 
