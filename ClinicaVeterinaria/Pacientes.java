7db65a70f473ea614ae587c96b85e94f9454b30c
Pollyana-Vieira <adenicecandidadossantosvieira@gmail.com>
2026-04-15 00:49:01 +0000

    criação de arquivo

--- ClinicaVeterinaria/Pacientes.java
+++ ClinicaVeterinaria/Pacientes.java
@@ -1,23 +1,24 @@
 import java.util.*;
 
 public class Pacientes {
-    
+
     public static String Animal(String){
 
-        private String nome;
-        private String especie;
-
-        public  String Animal(String nome, String especie){
-          
-            this.nome = nome;
-            this.espécie = especie;
-
-        }
-        void ExibirDadosDoPaciente(){
-            System.out.println("Nome" + nome);
-            System.out.println("Espécie" + especie);
-
-        }
+      private String nome;
+      private String especie;
     }
-    
+
+    public String Animal(String nome, String especie) {
+
+        this.nome = nome;
+        this.espécie = especie;
+
+    }
+
+    void ExibirDadosDoPaciente() {
+        System.out.println("Nome" + nome);
+        System.out.println("Espécie" + especie);
+
+    }
+
 }
