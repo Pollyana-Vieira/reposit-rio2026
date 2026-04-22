82fede632147fe71e4b21cdebaeb574c41556655
Pollyana-Vieira <adenicecandidadossantosvieira@gmail.com>
2026-04-22 13:31:42 +0000

    criação de arquivo

---
+++ ClinicaVeterinaria/Pilha.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Pilha{
+
+    public static void main(String[]args){
+
+        Stack<String> livros = new Stack<>();
+
+        livros.push("Corrupt");
+        livros.push("Hideaway");
+        livros.push("Kill Switch");
+        livros.push("Conclave");
+        livros.push("Nightfall");
+        livros.push("Fire Night");
+
+        for(String l : livros){
+
+           System.out.println(livros.peek());
+            System.out.println(l);
+        }
+        
+        
+       
+
+    }
+}
