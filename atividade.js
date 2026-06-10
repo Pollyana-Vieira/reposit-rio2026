af238089f37fed07b5ce1bfb3a0ebecf68e035a6
Pollyana-Vieira <adenicecandidadossantosvieira@gmail.com>
2026-06-10 00:32:45 +0000

    criação de arquivo

--- atividade.js
+++ atividade.js
@@ -1,12 +1,12 @@
 // Trecho 1
-let numero = 10;
-numero = "dez";
-console.log(numero);
+//let numero = 10;
+//numero = "dez";
+//console.log(numero);
 
 //Trecho 2
-let valor= 5 + "5";
-console.log(valor);
-console.log(typeof valor);
+//let valor= 5 + "5";
+//console.log(valor);
+//console.log(typeof valor);
 
 //Trecho 3
 
@@ -14,3 +14,4 @@ console.log(typeof valor);
 //Trecho 4
 let resultado= "100" - 50;
 console.log(resultado);
+console.log(typeof resultado);
