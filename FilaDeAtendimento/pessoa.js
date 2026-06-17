1084811eca7d78eda1c718c2de97dacae5311662
Pollyana-Vieira <adenicecandidadossantosvieira@gmail.com>
2026-06-17 19:22:35 +0000

    criação de arquivo

--- FilaDeAtendimento/pessoa.js
+++ FilaDeAtendimento/pessoa.js
@@ -1,4 +1,5 @@
-async function Pessoa(){
+class Pessoa(){
+    
     let nome = await ("Digite seu nome: "); 
     let idade = await ("Digite sua idade: ");
     let dataDeNascimento = await ("Digite sua data de nascimento: "); 
