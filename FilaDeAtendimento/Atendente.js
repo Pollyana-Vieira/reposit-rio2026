f8a38f1aad4176affbfd3fd3590193224b973140
Pollyana-Vieira <adenicecandidadossantosvieira@gmail.com>
2026-06-17 23:09:41 +0000

    criação de arquivo

--- FilaDeAtendimento/Atendente.js
+++ FilaDeAtendimento/Atendente.js
@@ -2,11 +2,12 @@ const Pessoa = require("./Pessoa");
 
 class Atendente extends Pessoa {
 
-    constructor(nome, cpf, dataDeNascimento, idade, matricula) {
+    constructor(nome, cpf, dataDeNascimento, idade, matricula, historico) {
 
         super(nome, cpf, dataDeNascimento, idade);
 
         this.matricula = matricula;
+        this.historico = [];
 
     }
     matricula() {
@@ -20,6 +21,17 @@ class Atendente extends Pessoa {
     atenderCliente(){
         console.log("\nAtender Clientes");
         console.log("Cliente: ", this.nome, this.tipo, this.senha);
+    }
+    historicoDoAtendente(){
+
+        console.log("Nome: ", this.nome);
+        console.log("Matricula: ", this.matricula);
+        console.log("Data de nascimento: ", this.dataDeNascimento);
+        console.log("Idade: ", this.idade);
+        console.log("CPF: ", this.cpf);
+
+        console.log("\nLista De Atendidos");
+        console.log
     }
 
 }
