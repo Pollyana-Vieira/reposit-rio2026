b9a18395e4151ce7501e97d22d248a40b7ccba64
Pollyana-Vieira <adenicecandidadossantosvieira@gmail.com>
2026-06-17 00:18:36 +0000

    criação de arquivo

---
+++ FilaDeAtendimento/pessoa.js
@@ -0,0 +1,7 @@
+const readline = require("readline");
+
+async function Pessoa{
+    let nome = await rl.question("Digite seu nome: "); 
+    let idade = await rl.question("Digite sua idade: ");
+    let 
+} 
