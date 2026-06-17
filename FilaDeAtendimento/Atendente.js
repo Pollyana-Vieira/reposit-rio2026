20b938052ba6d1b5c18dfd951c90d179ed3d3d1a
Pollyana-Vieira <adenicecandidadossantosvieira@gmail.com>
2026-06-17 22:52:20 +0000

    criação de arquivo

--- FilaDeAtendimento/Atendente.js
+++ FilaDeAtendimento/Atendente.js
@@ -1,7 +1,20 @@
-//pessoa com matricula
-//histórico de todos que já atendeu
-//possível ver um relatório completo do atendente
+const Pessoa = require("./Pessoa");
 
-class atendente extends Pessoa(){
+class Atendente extends Pessoa() {
+
+    constructor(matricula) {
+
+        super(nome, cpf, dataDeNascimento, idade);
+
+        this.matricula = 0000001;
+
+    }
+    if(this.matricula =! null){
+       return "Matricula" + string(Atendente.matricula++); 
+    }
+    if else(this.matricula === undefined){
+        return "Matricula não declarada";
+    }
     
+
 }
